e3c6c23e65a89399d15ca661fd3b76c18329d8d5
Pascal MARTIN <contact@pascal-martin.fr>
2016-08-22 21:44:47 +0200

    Demo01: C++ source file

---
+++ demo01/demo01.cpp
@@ -0,0 +1,37 @@
+#include "inkview.h"
+
+static const int kFontSize = 42;
+
+static int main_handler(int event_type, int param_one, int param_two)
+{
+    if (EVT_INIT == event_type) {
+        ifont *font = OpenFont("LiberationSans", kFontSize, 0);
+
+        // Effacement de l'écran
+        ClearScreen();
+
+        // Affichages sur un buffer
+        SetFont(font, BLACK);
+        DrawLine(0, 25, ScreenWidth(), 25, 0x00333333);
+        DrawLine(0, ScreenHeight() - 25, ScreenWidth(), ScreenHeight() - 25, 0x00666666);
+        FillArea(50, 250, ScreenWidth() - 50*2, ScreenHeight() - 250*2, 0x00E0E0E0);
+        FillArea(100, 300, ScreenWidth() - 100*2, ScreenHeight() - 300*2, 0x00A0A0A0);
+        DrawTextRect(0, ScreenHeight()/2 - kFontSize/2, ScreenWidth(), kFontSize, "Hello, world!", ALIGN_CENTER);
+
+        // Copie du buffer vers l'écran
+        FullUpdate();
+
+        CloseFont(font);
+    }
+    else if (EVT_KEYPRESS == event_type) {
+        CloseApp();
+    }
+    return 0;
+}
+
+
+int main (int argc, char* argv[])
+{
+    InkViewMain(main_handler);
+    return 0;
+}
