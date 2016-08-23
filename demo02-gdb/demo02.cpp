ce9c987882063b73be214279fae168f3407f0186
Pascal MARTIN <pmartin@tea-ebook.com>
2016-08-23 13:03:53 +0200

    Demo02: C++ code of a demo application, with a function that uses a few variables and displays them too

---
+++ demo02-gdb/demo02.cpp
@@ -0,0 +1,57 @@
+#include "inkview.h"
+
+static const int kFontSize = 16;
+
+
+static int test_debug(ifont *font, int a, int b)
+{
+    char buffer[1024];
+    int result = 0;
+
+    sprintf(buffer, "a=%d b=%d result=%d", a, b, result);
+    DrawTextRect(0, 0, ScreenWidth(), kFontSize, buffer, ALIGN_LEFT);
+    FullUpdate();
+
+    a += 10;
+
+    sprintf(buffer, "a=%d b=%d result=%d", a, b, result);
+    DrawTextRect(0, 20, ScreenWidth(), kFontSize, buffer, ALIGN_LEFT);
+    FullUpdate();
+
+    b *= 2;
+
+    sprintf(buffer, "a=%d b=%d result=%d", a, b, result);
+    DrawTextRect(0, 40, ScreenWidth(), kFontSize, buffer, ALIGN_LEFT);
+    FullUpdate();
+
+    result = a * b;
+    sprintf(buffer, "a=%d b=%d result=%d", a, b, result);
+    DrawTextRect(0, 60, ScreenWidth(), kFontSize, buffer, ALIGN_LEFT);
+    FullUpdate();
+}
+
+
+static int main_handler(int event_type, int param_one, int param_two)
+{
+    if (EVT_INIT == event_type) {
+        ifont *font = OpenFont("LiberationSans", kFontSize, 0);
+
+        ClearScreen();
+        SetFont(font, BLACK);
+
+        test_debug(font, 1000, 3);
+
+        CloseFont(font);
+    }
+    else if (EVT_KEYPRESS == event_type) {
+        CloseApp();
+    }
+    return 0;
+}
+
+
+int main (int argc, char* argv[])
+{
+    InkViewMain(main_handler);
+    return 0;
+}
