279ef9cb44e5819b81bd97f3e5a4dfbad073f597
Pascal MARTIN <pmartin@tea-ebook.com>
2016-08-29 08:55:06 +0200

    Demo06 : context-menu : tried a few more things...
    Doesn't seem to segfault, now; but only the title of the
    menu is displayed: items are not ;-(

--- demo06-ui/demo06.cpp
+++ demo06-ui/demo06.cpp
@@ -1,5 +1,7 @@
 #include "inkview.h"
 
+static ifont *font;
+
 
 // Seems like DialogSynchro() is blocking : code execution will only continue after
 // a button has been clicked
@@ -52,48 +54,126 @@ static void menu_01()
 
 
 
+char *submenu2_text;
+char *submenu1_text;
+char *menu_text;
+
+icontext_menu *context_menu;
+icontext_menu_properties *properties = NULL;
+
+struct imenu_s *submenu1 = NULL, *submenu2 = NULL;
+imenu *menu;
+
+irect *rect_pos_selected_item;
+irect *rect_pos_menu;
+
+
+static void contextmenu_01_handler(int index)
+{
+	char buffer[2048];
+	snprintf(buffer, 2048, "Context Menu: index %d has been selected!", index);
+	Message(ICON_INFORMATION, "Debug", buffer, 3*1000);
+
+	free(properties);
+
+	free(submenu2);
+	free(submenu1);
+	free(menu);
+
+	free(rect_pos_selected_item);
+	free(rect_pos_menu);
+
+	free(submenu2_text);
+	free(submenu1_text);
+	free(menu_text);
+}
+
+
 static void context_menu_01()
 {
-	/*
-	imenuex menuex;
-	menuex.type = 0;
-	menuex.index = 0;
-	menuex.text = "1st menu item?";
-	menuex.submenu = NULL;
-	menuex.icon = NULL;
-	menuex.font = font;
-	*/
+	rect_pos_selected_item = (irect *)malloc(sizeof(irect));
+	rect_pos_selected_item->x = 200;
+	rect_pos_selected_item->y = 200;
+	rect_pos_selected_item->w = 10;
+	rect_pos_selected_item->h = 50;
+	rect_pos_selected_item->flags = 0;
 
-	struct imenu_s submenu3 = {.type = 3, .index = 3, .text = (char *)"Text of entry 3 (type=3)", .submenu = NULL};
-	struct imenu_s submenu2 = {.type = 2, .index = 2, .text = (char *)"Text of entry 2 (type=2)", .submenu = &submenu3};
-	struct imenu_s submenu1 = {.type = 1, .index = 1, .text = (char *)"Text of entry 1 (type=1)", .submenu = &submenu2};
+	rect_pos_menu = (irect *)malloc(sizeof(irect));
+	rect_pos_menu->x = 100;
+	rect_pos_menu->y = 100;
+	rect_pos_menu->w = 400;
+	rect_pos_menu->h = 600;
+	rect_pos_menu->flags = 0;
 
-	// If type != 0 => it segfaults when calling OpenContextMenu()
-	// But type == 0 doesn't display the menu (doesn't segfault, but does pretty much nothing?)
-	imenu menu = {.type = 1, .index = 0, .text = (char *)"My menu?", .submenu = &submenu1};
+	submenu2_text = (char *)malloc(strlen("Text of entry 2 (type=2)") + 1);
+	strcpy(submenu2_text, "Text of entry 2 (type=2)");
+	submenu2 = (imenu_s *)malloc(sizeof(struct imenu_s));
+	submenu2->type = 2;
+	submenu2->index = 2;
+	submenu2->text = submenu2_text;
+	submenu2->submenu = NULL;
 
-	icontext_menu_properties properties;
-	//properties.font_normal = font;
-	//properties.font_selected = font;
-	//properties.font_disabled = font;
-	// A voir : plein d'autres champs...
+	submenu1_text = (char *)malloc(strlen("Text of entry 1 (type=2)") + 1);
+	strcpy(submenu1_text, "Text of entry 1 (type=2)");
+	submenu1 = (imenu_s *)malloc(sizeof(struct imenu_s));
+	submenu1->type = 2;
+	submenu1->index = 1;
+	submenu1->text = submenu1_text;
+	submenu1->submenu = submenu2;
 
+	menu_text = (char *)malloc(strlen("My menu?") + 1);
+	strcpy(menu_text, "My menu?");
+	menu = (imenu *)malloc(sizeof(imenu));
+	menu->type = 1;
+	menu->index = 0;
+	menu->text = menu_text;
+	menu->submenu = submenu1;
 
-	icontext_menu *context_menu = CreateContextMenu("my_menu");
-	context_menu->hproc = (iv_menuhandler)menu_01_handler;
+	properties = (icontext_menu_properties *)malloc(sizeof(icontext_menu_properties));
+	properties->font_normal = font;
+	properties->font_selected = font;
+	properties->font_disabled = font;
+	properties->background_color_normal = WHITE;
+	properties->background_color_disabled = LGRAY;
+	properties->blackout = 0;
+	properties->item_height = 30;
+	properties->item_border_indent_horizontal = 2;
+	properties->separator_indent_horizontal = 5;
+	properties->separator_indent_vertical = 3;
+	properties->separator_height = 6;
+	properties->separator_style = 1;
+	properties->separator_color = DGRAY;
+	properties->separator_frequency = 1;
+	properties->marker_height = 10;
+	properties->marker_indent_horizontal = 5;
+	properties->marker_style = 1;
+	properties->marker_color = LGRAY;
+	properties->border_size = 2;
+	properties->border_radius = 5;
+	properties->title_height = 60;
+	properties->enable_pointer = 1;
+	properties->font_normal_color = BLACK;
+	properties->font_selected_color = DGRAY;
+	properties->font_disabled_color = LGRAY;
+	properties->bullet_show = 1;
+	properties->bullet_style = 1;
+	properties->item_border_indent_vertical = 4;
+	properties->background_style = 2;
 
-	//context_menu->menuex = &menuex;
-	context_menu->menu = &menu;
+	context_menu = CreateContextMenu("menu");
+	context_menu->hproc = (iv_menuhandler)contextmenu_01_handler;
 
-	context_menu->pos_selected_item = GetMenuRect(&menu);
-	context_menu->pos_menu = GetMenuRect(&menu);
-	context_menu->properties = &properties;
-	context_menu->active_element = 0;
+	context_menu->menu = menu;
+
+	context_menu->pos_selected_item = *rect_pos_selected_item;
+	context_menu->pos_menu = *rect_pos_menu;
+
+	context_menu->properties = properties;
+	context_menu->active_element = 2;
 	context_menu->enable_pointer = 1;
-	context_menu->enable_aura = 1;
+	context_menu->enable_aura = 0;
 	context_menu->use_own_font = 0;
-	context_menu->update_after_close = 0;
-
+	context_menu->update_after_close = 1;
 
 	OpenContextMenu(context_menu);
 	SetContextMenu(context_menu);
@@ -111,7 +191,8 @@ static int main_handler(int event_type, int param_one, int param_two)
 
 	switch (event_type) {
 	case EVT_INIT:
-
+		font = OpenFont("LiberationSans", 24, 1);
+		SetMenuFont(font);
 		break;
 	case EVT_SHOW:
 
@@ -149,7 +230,7 @@ static int main_handler(int event_type, int param_one, int param_two)
 
 		break;
 	case EVT_EXIT:
-
+		CloseFont(font);
 		break;
 	default:
 		break;
